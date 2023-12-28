ced3ed245ac11aef034f271484c41309b20f0391
Majid Kargar <majidkargar01@gmail.com>
2023-12-28 22:31:32 +0330

    init

---
+++ lua/fulcain/packer.lua
@@ -0,0 +1,28 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	-- telescope (Fuzzy finder)
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	-- Rose pine color scheme
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function ()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+end)
