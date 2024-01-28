07dc737f477ba77fbd0195e2c486184328e61a14
Majid Kargar <majidkargar01@gmail.com>
2024-01-28 20:54:22 +0330

    feat: Add jsdoc comment binding

--- lua/fulcain/remap.lua
+++ lua/fulcain/remap.lua
@@ -46,3 +46,7 @@ vim.keymap.set('', '<up>', '<nop>')
 vim.keymap.set('', '<down>', '<nop>')
 vim.keymap.set('', '<left>', '<nop>')
 vim.keymap.set('', '<right>', '<nop>')
+
+
+-- JSDoc comment
+vim.keymap.set("n", "<leader>jd", "O/** */<ESC>hhi<ENTER><BACKSPACE><BACKSPACE><ESC>O<ESC>")  
