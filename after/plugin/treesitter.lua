67b14d497e3fed76760368cb1708afd5887f1f25
Majid Kargar <majidkargar01@gmail.com>
2024-02-04 21:01:48 +0330

    Remove: uninstall some treesitter stuff

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,22 +1,12 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { 
-      "javascript", 
-      "typescript",
       "c", 
       "lua", 
       "vim", 
       "vimdoc", 
       "query", 
-      "css", 
-      "html", 
-      "jsdoc", 
-      "json",
-      "json5",
-      "markdown_inline",
-      "scss"
   },
-
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
