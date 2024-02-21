c6197042c20135f0ed9d281cc5220f081c4fcdc7
Majid Kargar <majidkargar01@gmail.com>
2024-02-22 00:06:00 +0330

    Comment index rainbow plugin

--- lua/plugins/indet-blank-line.lua
+++ lua/plugins/indet-blank-line.lua
@@ -1,17 +1,17 @@
-return{
-    'lukas-reineke/indent-blankline.nvim',
-    dependencies = {
-        'theglander/indent-rainbowline.nvim',
-    },
-    config = function()
-        opts = {}
-        -- other blankline configuration here
-        require('ibl').setup(require('indent-rainbowline').make_opts(opts, {
-            -- how transparent should the rainbow colors be. 1 is completely opaque, 0 is invisible. 0.07 by default
-            color_transparency = 0.15,
-            -- the 24-bit colors to mix with the background. specified in hex.
-            -- { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec, } by default
-            colors = { 0xff0000, 0x00ff00, 0x0000ff, },
-        }))
-    end,
+return {
+	-- 'lukas-reineke/indent-blankline.nvim',
+	-- dependencies = {
+	-- 	'theglander/indent-rainbowline.nvim',
+	-- },
+	-- config = function()
+	-- 	opts = {}
+	-- 	-- other blankline configuration here
+	-- 	require('ibl').setup(require('indent-rainbowline').make_opts(opts, {
+	-- 		-- how transparent should the rainbow colors be. 1 is completely opaque, 0 is invisible. 0.07 by default
+	-- 		color_transparency = 0.1,
+	-- 		-- the 24-bit colors to mix with the background. specified in hex.
+	-- 		-- { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec, } by default
+	-- 		colors = { 0xff0000, 0x00ff00, 0x0000ff, },
+	-- 	}))
+	-- end,
 }
