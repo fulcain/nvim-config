44479d605ff3dd7005ecd08e7023756296bf7cb8
Majid Kargar <majidkargar01@gmail.com>
2024-02-22 00:02:23 +0330

    update treesitter

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,49 +1,46 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        'windwp/nvim-ts-autotag',
-    },
-    config = function()
-        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+	'nvim-treesitter/nvim-treesitter',
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter-textobjects',
+		'windwp/nvim-ts-autotag',
+	},
+	build = ":TSUpdate",
+	event = {"BufReadPre", "BufNewFile"},
+	config = function()
+		require'nvim-treesitter.configs'.setup {
+			ensure_installed = { 
+				"c", 
+				"lua", 
+				"vim", 
+				"vimdoc", 
+				"query", 
+				"tsx",
+				"json"
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>", 
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+			autotag = {
+				enable = true,
+			},
+			sync_install = false,
+			auto_install = true,
+			indent = {
+				enable = true,
+				disable = {},
+			},       
+			highlight = {
+				enable = true,
+				disable = {},
 
-        ts_update()
-
-        require'nvim-treesitter.configs'.setup {
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = { 
-                "c", 
-                "lua", 
-                "vim", 
-                "vimdoc", 
-                "query", 
-                "tsx",
-                "json"
-            },
-            autotag = {
-                enable = true,
-            },
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            indent = {
-                enable = true,
-                disable = {},
-            },       
-            highlight = {
-                enable = true,
-                disable = {},
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-        }
-    end
+				additional_vim_regex_highlighting = false,
+			},
+		}
+	end
 }
