b8189096f4f139de7133c93a52d5eb8e89cdb307
Majid Kargar <majidkargar@Majids-Laptop.local>
2024-04-13 23:03:17 +0330

    Enable hidden files in oil by default

--- lua/plugins/oil-nvim.lua
+++ lua/plugins/oil-nvim.lua
@@ -5,7 +5,28 @@ return {
 	config = function()
 		require("oil").setup({
 			default_file_explorer = true,
-		})
-	end
-}
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+				-- This function defines what is considered a "hidden" file
+				is_hidden_file = function(name, bufnr)
+					return vim.startswith(name, ".")
+				end,
+				-- This function defines what will never be shown, even when `show_hidden` is set
+				is_always_hidden = function(name, bufnr)
+					return false
+				end,
+				-- Sort file names in a more intuitive order for humans. Is less performant,
+				-- so you may want to set to false if you work with large directories.
+				natural_order = false,
+				sort = {
+					-- sort order can be "asc" or "desc"
+					-- see :help oil-columns to see which columns are sortable
+					{ "type", "asc" },
+					{ "name", "asc" },
+				},
+			},
 
+		})
+	end,
+}
