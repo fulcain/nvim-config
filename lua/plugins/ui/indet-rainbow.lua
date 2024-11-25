e7f9c23f179bce21e0b4e2e5caab6a28b5d8fff1
Majid Kargar <majidkargar01@gmail.com>
2024-11-26 02:34:35 +0330

    Feat: enable index-rainbow

--- lua/plugins/ui/indet-rainbow.lua
+++ lua/plugins/ui/indet-rainbow.lua
@@ -2,7 +2,11 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	main = "ibl",
+	branch = "lua",
 	opts = {
 		indent = { char = "┊" },
 	},
+	config = function()
+		require("ibl").setup()
+	end,
 }
