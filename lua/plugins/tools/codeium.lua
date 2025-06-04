24397a0daec78f8f8aab78e62391c86e1dc28835
Majid Kargar <majidkargar01@gmail.com>
2025-06-04 11:52:01 +0330

    refactor: enable codeium

--- lua/plugins/tools/codeium.lua
+++ lua/plugins/tools/codeium.lua
@@ -4,7 +4,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		"hrsh7th/nvim-cmp",
 	},
-	enabled = false,
+	-- enabled = false,
 	config = function()
 		require("codeium").setup({
 			-- Optionally disable cmp source if using virtual text only
